f8583944a14d5bdaeefa64a7ab17dfee087b2936
mkalawska <magda.kalawska98@gmail.com>
2021-09-08 13:30:40 +0200

    Add iteration

--- signal_acc/signal_acc.srcs/sim_1/new/mb_design_tb.v
+++ signal_acc/signal_acc.srcs/sim_1/new/mb_design_tb.v
@@ -26,24 +26,33 @@ reg reset, reset_n;
 wire [11:0] A, B, C, R;
 wire [31:0] RE, IM;
 real iter = 0;
-real r_a = 1023;
-real r_b = 589;
-real r_c = 653;
-real r_r = 110;
+real r_a;
+real r_b; 
+real r_c;
+real r_r;
 
+         
  // Dip switches stimulus
- assign A = r_a;
- assign B = r_b;
- assign C = r_c;
- assign R = r_r;
+assign A = r_a * iter;
+assign B = r_b * iter;
+assign C = r_c * iter;
+assign R = r_r * iter;
+ 
  // Reset stimulus
  initial
- begin
- reset = 1'b1;
- reset_n = 1'b0;
- #10 reset = 1'b0;
- reset_n = 1'b1;
+      begin
+         reset = 1'b1;
+         reset_n = 1'b1;
+        #10 reset = 1'b0;
+         reset_n = 1'b1;
+         
+         end
+        
+ always
+ begin 
+ #20000 reset = ~reset;
  end
+ 
  // Clocks stimulus
  initial
  begin
@@ -55,6 +64,19 @@ real r_r = 110;
  #5 clk_n = ~clk_n; //toggle clk every 5 time units
  clk_p = ~clk_p; //toggle clk every 5 time units
  end
+     //increase iter
+     //Signals stimuli
+always @ (posedge reset)
+    begin
+    #20000
+    if (iter < 5 ) iter = iter + 1; else iter = 1;
+         r_a = 123 * iter;
+         r_b = 102 * iter;
+         r_c = 98 * iter;
+         r_r = 30 * iter;
+   
+   end
+
  //Put sin and cos as real values
 // always @*
  //begin
@@ -65,4 +87,5 @@ real r_r = 110;
  //end
  //Instantiate tested module
  mb_design_wrapper mb_design_inst ( clk_n, clk_p, A, B, C, IM, RE, R, reset, reset_n);
+ 
  endmodule
